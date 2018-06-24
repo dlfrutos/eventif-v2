0126ddbed17193fa0a373ee783ec2b2aacd000bd
Daniel <dlfrutos@hotmail.com>
2018-06-24 20:54:50 -0300

    Inicio das alterações para login com email

---
+++ app/src/main/java/br/edu/ifsc/eventos/activities/EmailLoginActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsc.eventos.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_login)
+    }
+}
